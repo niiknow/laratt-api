5cd27fe104e520b67f20582d599016937e0e2be6
noogen <tom@noogen.net>
2018-10-24 13:24:52 -0500

    update conf

--- config/admin.php
+++ config/admin.php
@@ -3,10 +3,14 @@
 return [
     'auditable' => [
         'bucket' => env('AWS_BUCKET_AUDITABLE'),
+        'include' => [
+            'tables' => '.*',  // include all
+            'tenants' => '.*'  // include all
+        ],
         'exclude' => [
-            'tables' => ['log', 'cache'],
-            'tenants' => ['demo', 'test']
-        ]
+            'tables' => '(log.*|cache)', // exclude log2015 or cache
+            'tenants' => '(demo|test)'   // exclude demo or test tenant
+        ],
     ],
     'token' => env('ADMIN_TOKEN')
 ];
