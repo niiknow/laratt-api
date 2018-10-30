295952233ad4ef08d6cb3bf1e2b0fe9a68855218
noogen <tom@noogen.net>
2018-10-30 16:00:38 -0500

    add init

--- public/init.php
+++ public/init.php
@@ -1,7 +1,43 @@
 <?php
+/**
+ * This file is use to initialize laravel app on shared hosting servers; i.e.
+ * servers that do not allow shell_exec or exec.  This allow for using
+ * rachidlaasri/laravel-installer package by visiting /install
+ *
+ * Therefore, all configurations are now done from the front-end.
+ *
+ */
+$rootPath = __DIR__ . '/..';
+$envFile  = $rootPath . '/.env';
 
-if (!file_exists("../.env")) {
-    $output = exec('cd .. && ./webinit.sh');
+function changeMods($path)
+{
+    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+    foreach ($iterator as $item) {
+        @chmod($item, 0775);
+    }
+}
+
+function genuid($l = 32)
+{
+    return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz"), 0, $l);
+}
+
+if (!file_exists($envFile)) {
+    copy($envFile . '.example', $envFile);
+
+    @changeMods(realpath($rootPath . '/storage/framework/'));
+    @changeMods(realpath($rootPath . '/storage/logs/'));
+    @changeMods(realpath($rootPath . '/bootstrap/cache/'));
+
+    require __DIR__.'/../vendor/autoload.php';
+
+    // generate APP_ENV and API_KEY
+    $file = file_get_contents($envFile);
+    $file = preg_replace("/APP_KEY=.*/", "APP_KEY=base64:".base64_encode(genuid()), $file, 1);
+    $file = preg_replace("/API_KEY=.*/", "API_KEY=".genuid(), $file, 1);
+    file_put_contents($envFile, $file);
 }
 
 header('Location: install');
