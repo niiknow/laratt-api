5a30442c0768001f4dbc4b61ff71c41d548db73d
noogen <tom@noogen.net>
2019-05-15 15:39:45 -0500

    fix init

--- public/init.php
+++ public/init.php
@@ -9,6 +9,7 @@
  */
 $rootPath = __DIR__ . '/..';
 $envFile  = $rootPath . '/.env';
+$initFile = $rootPath . '/.init';
 
 function changeMods($path)
 {
@@ -19,28 +20,28 @@ function changeMods($path)
     }
 }
 
-/**
- * @param $l
- */
-function genuid($l = 32)
+function genuid()
 {
-    return substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyz'), 0, $l);
+    return substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyz'), 0, 32);
 }
 
-if (!file_exists($envFile)) {
-    copy($envFile . '.example', $envFile);
+if (!file_exists($initFile)) {
+    if (!file_exists($envFile)) {
+        copy($envFile . '.example', $envFile);
+    }
 
     @changeMods(realpath($rootPath . '/storage/framework/'));
     @changeMods(realpath($rootPath . '/storage/logs/'));
     @changeMods(realpath($rootPath . '/bootstrap/cache/'));
-
-    require __DIR__ . '/../vendor/autoload.php';
 
     // generate APP_ENV and API_KEY
     $file = file_get_contents($envFile);
     $file = preg_replace('/APP_KEY=.*/', 'APP_KEY=base64:' . base64_encode(genuid()), $file, 1);
     $file = preg_replace('/API_KEY=.*/', 'API_KEY=' . genuid(), $file, 1);
     file_put_contents($envFile, $file);
+
+    $now = new \DateTime();
+    file_put_contents($initFile, $date->format('Y-m-d H:i:s'));
 }
 
 header('Location: install');
