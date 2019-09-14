9e8762a9e484a59be0ad6df3df4ab670a0808f3d
noogen <tom@noogen.net>
2019-09-14 15:00:43 -0500

    update to Laravel 6.0

--- config/cache.php
+++ config/cache.php
@@ -31,58 +31,59 @@ return [
     |
     */
 
-    'stores' => [
+    'stores'  => [
 
-        'apc' => [
-            'driver' => 'apc',
+        'apc'       => [
+            'driver' => 'apc'
         ],
 
-        'array' => [
-            'driver' => 'array',
+        'array'     => [
+            'driver' => 'array'
         ],
 
-        'database' => [
-            'driver' => 'database',
-            'table' => 'cache',
-            'connection' => null,
+        'database'  => [
+            'driver'     => 'database',
+            'table'      => 'cache',
+            'connection' => null
         ],
 
-        'file' => [
+        'file'      => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+            'path'   => storage_path('framework/cache/data')
         ],
 
         'memcached' => [
-            'driver' => 'memcached',
+            'driver'        => 'memcached',
             'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-            'sasl' => [
+            'sasl'          => [
                 env('MEMCACHED_USERNAME'),
-                env('MEMCACHED_PASSWORD'),
+                env('MEMCACHED_PASSWORD')
             ],
-            'options' => [
+            'options'       => [
                 // Memcached::OPT_CONNECT_TIMEOUT => 2000,
             ],
-            'servers' => [
+            'servers'       => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
-                    'weight' => 100,
-                ],
-            ],
+                    'host'   => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port'   => env('MEMCACHED_PORT', 11211),
+                    'weight' => 100
+                ]
+            ]
         ],
 
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'cache',
+        'redis'     => [
+            'driver'     => 'redis',
+            'connection' => 'cache'
         ],
 
-        'dynamodb' => [
-            'driver' => 'dynamodb',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'table' => env('DYNAMODB_CACHE_TABLE', 'cache'),
-        ],
+        'dynamodb'  => [
+            'driver'   => 'dynamodb',
+            'key'      => env('AWS_ACCESS_KEY_ID'),
+            'secret'   => env('AWS_SECRET_ACCESS_KEY'),
+            'region'   => env('AWS_DEFAULT_REGION', 'us-east-1'),
+            'table'    => env('DYNAMODB_CACHE_TABLE', 'cache'),
+            'endpoint' => env('DYNAMODB_ENDPOINT')
+        ]
 
     ],
 
@@ -96,7 +97,6 @@ return [
     | value to get prefixed to all our keys so we can avoid collisions.
     |
     */
-
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    'prefix'  => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_cache')
 
 ];
